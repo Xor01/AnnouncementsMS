0b2aa16054568502d16b7d8349b83fdabffaf91b
Xor01 <ms3ccc71@gmail.com>
2023-05-20 19:54:42 +0300

    Login panel is fully centered and aligned

---
+++ src/Login.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+
+    private JLabel loginLabel, usernameLabel, passwordLabel;
+
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginBtn;
+    private JButton registerBtn;
+
+    public Login () {
+        setLayout(new GridBagLayout());
+        setTitle("Welcome - Login");
+        loginLabel = new JLabel("Welcome Enter your credentials or click register to signup");
+        loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        loginLabel.setVerticalAlignment(SwingConstants.CENTER);
+        loginBtn = new JButton("Login");
+        registerBtn = new JButton("Register");
+
+        usernameLabel = new JLabel("Username");
+        passwordLabel = new JLabel("Password");
+
+        usernameField = new JTextField(20);
+        passwordField = new JPasswordField(20);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 0, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.CENTER;
+//        login label
+        gbc.gridy = 0;
+        gbc.gridx = 1;
+        gbc.gridwidth = 4;
+        add(loginLabel, gbc);
+
+//        username label
+        gbc.gridy = 1;
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        add(usernameLabel, gbc);
+
+//        username field
+        gbc.gridy = 1;
+        gbc.gridx = 3;
+        gbc.gridwidth = 2;
+        add(usernameField, gbc);
+
+//        password label
+        gbc.gridy = 2;
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        add(passwordLabel, gbc);
+
+
+//        password field
+        gbc.gridy = 2;
+        gbc.gridx = 3;
+        gbc.gridwidth = 2;
+        add(passwordField, gbc);
+
+
+//        register button
+        gbc.gridy = 3;
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+
+        gbc.anchor = GridBagConstraints.WEST;
+        add(registerBtn, gbc);
+
+//        login button
+        gbc.gridy = 3;
+        gbc.gridx = 3;
+        gbc.gridwidth = 2;
+        add(loginBtn, gbc);
+
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginLabel.setText("You are logged in");
+            }
+        });
+
+        registerBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginLabel.setText("You are going to signup");
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setSize(500, 500);
+        login.setVisible(true);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setSize(750, 500);
+        l.setVisible(true);
+        l.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}

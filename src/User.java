7bfb0e84a608663fa00ad1364c73d0604e6b66e6
Xor01 <ms3ccc71@gmail.com>
2023-05-27 19:58:25 +0300

    changing textField to textPane and style it

--- src/User.java
+++ src/User.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -49,11 +50,16 @@ public class User extends JFrame {
 
         JPanel typingPanel = new JPanel(new BorderLayout());
         JButton sendBtn = new JButton("Send");
+        if (!isAdmin){
+            sendBtn.setEnabled(false);
+            sendBtn.setToolTipText("Only admin can send");
+        }
         TextArea type = new TextArea();
 
         typingPanel.add(type, BorderLayout.CENTER);
         typingPanel.add(sendBtn, BorderLayout.EAST);
         add(typingPanel, BorderLayout.SOUTH);
+
     }
 
     /**
@@ -62,15 +68,25 @@ public class User extends JFrame {
     private void addGroups(){
         try{
             ResultSet re = con.prepareStatement(
-                    "SELECT  * FROM  agroups, groupmembers where groupmembers.member_id = '1'", this.id
+                    String.format(
+                            "SELECT  * " +
+                                    "FROM  agroups, groupmembers" +
+                                    " where groupmembers.member_id = %d " +
+                                    "and agroups.id = groupmembers.group_id", this.id
+                    )
             ).executeQuery();
 
             while (re.next()){
-                groupsTabs.addTab(re.getString("gName"), new JPanel());
+                JTextPane panel = new JTextPane();
+                panel.setEditable(false);
+                groupsTabs.addTab(re.getString("gName"), panel);
+                int group_id = re.getInt("group_id");
+                loadMessages(group_id, panel);
             }
 
         }
         catch (SQLException e){
+            System.out.println(e);
             JOptionPane.showMessageDialog(this, "An error happened");
         }
     }
@@ -78,8 +94,49 @@ public class User extends JFrame {
     /**
      * this method will load each message for each group
      */
-    public void loadMessages(ResultSet group) {
+    public void loadMessages(int group_id, JTextPane panel) {
+        // Create a StyledDocument to hold the text and styles
+        StyledDocument document = panel.getStyledDocument();
 
+        // Create a style for the sender name
+        Style senderStyle = panel.addStyle("SenderStyle", null);
+        StyleConstants.setForeground(senderStyle, Color.RED);  // Set the text color
+        StyleConstants.setBold(senderStyle, true);  // Set the font weight
+
+        // Create a style for the created at timestamp
+        Style timestampStyle = panel.addStyle("TimestampStyle", null);
+        StyleConstants.setForeground(timestampStyle, Color.GRAY);  // Set the text color
+        StyleConstants.setItalic(timestampStyle, true);  // Set the font style
+        try{
+            ResultSet re = con.prepareStatement(String.format(
+                    "SELECT  distinct * FROM  messages, users where messages.group_id = %d and users.id = messages.sender_id", group_id
+                    )
+
+            ).executeQuery();
+
+            while (re.next()){
+                String message = String.format(
+                        "Sender: %s%nMessage: %s%n created at: %s%n",
+                re.getString("username"), re.getString("content"), re.getString("created_at"));
+                insertChatMessage(document, re.getString("username"), re.getString("content"), re.getString("created_at"), senderStyle, timestampStyle);
+                panel.add(new JTextField(message, panel.getWidth()));
+            }
+
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(this, "An error happened");
+            System.out.println(e);
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void insertChatMessage(StyledDocument document, String sender, String content, String timestamp, Style senderStyle, Style timestampStyle) throws BadLocationException {
+
+
+        document.insertString(document.getLength(), sender + ": ", senderStyle);
+        document.insertString(document.getLength(), content + "\n", null);
+        document.insertString(document.getLength(), "  " + timestamp + "\n", timestampStyle);
     }
     public static void main(String[] args) {
 //        User u =  new User(null);

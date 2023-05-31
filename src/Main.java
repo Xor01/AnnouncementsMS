9566180a702a4c36b92d7eefdd6c9ed7cd9456bb
Mohammed A <=>
2023-05-31 17:06:09 +0300

    Adding Flatlaf theme

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-import javax.swing.JFrame;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        FlatLightLaf.setup();
+        try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
         Login l = new Login();
         l.setSize(500, 500);
         l.setVisible(true);

8fde441201959470a9c1ef6e22a685e8bf96b99a
Mohammed A <=>
2023-06-05 20:11:51 +0300

    using invoke later

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
+import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         FlatMacLightLaf.registerCustomDefaultsSource("style");
         FlatMacLightLaf.setup();
-        Login l = new Login();
-        l.setSize(500, 500);
-        l.setVisible(true);
-        l.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        SwingUtilities.invokeLater(() -> {
+            Login l = new Login();
+            l.setSize(500, 500);
+            l.setVisible(true);
+            l.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        });
     }
 }

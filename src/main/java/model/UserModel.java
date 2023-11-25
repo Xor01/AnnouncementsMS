b99fe7721780ca03cf41551574eb1f7ac649062e
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-11-25 16:27:22 +0300

    update user model

--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -1,21 +1,22 @@
 package model;
 
-import controller.LoginController;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserModel {
-    private final String username;
+    private String username;
     private String firstName;
     private String lastName;
     private String email;
-    private final String password;
+    private String password;
     private int id;
 
-    public UserModel(String username, String firstname, String lastName, String email, String password) throws NoSuchAlgorithmException {
+    public UserModel(int id, String username, String firstname, String lastName, String email, String password) throws NoSuchAlgorithmException {
+        this.id = id;
         this.username = username.trim();
         this.firstName = firstname.trim();
         this.lastName = lastName.trim();
@@ -28,21 +29,42 @@ public class UserModel {
         this.password = PasswordHandler.encrypt(password.trim());
     }
 
-    public String getUsername(){
-        return this.username;
+    public UserModel(){}
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
+    public void setPassword(String password) throws NoSuchAlgorithmException {
+        this.password = PasswordHandler.encrypt(password.trim());
+    }
 
-    public ResultSet login() throws SQLException {
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserModel login() throws SQLException {
         Connection con = ConnectionManager.getConnection();
-        String query = "Select id, username, pass from Users where username = ? and pass = ? ";
+        String query = "Select id, username, pass, fname, lname, email from Users where username = ? and pass = ? ";
         PreparedStatement loginPreparedStatement = con.prepareStatement(query);
-        loginPreparedStatement.setString(1, username);
-        loginPreparedStatement.setString(2, password);
+        loginPreparedStatement.setString(1, this.username);
+        loginPreparedStatement.setString(2, this.password);
         ResultSet userResult = loginPreparedStatement.executeQuery();
 
         if (userResult.next()){
-            return userResult;
+            this.id = userResult.getInt("id");
+            this.firstName = userResult.getString("fname");
+            this.lastName = userResult.getString("lname");
+            this.email = userResult.getString("email");
+            return this;
         }
         else {
             System.out.println("Login failed");
@@ -50,16 +72,18 @@ public class UserModel {
         return null;
     }
 
-    public ResultSet register(){
+    public boolean register(){
         String regex = "^(.+)@(.+)$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            System.out.println("Atleast one empty field exist");
+            System.out.println("At least one empty field exist");
+            return false;
         }
 
         else if (!matcher.matches()) {
             System.out.println("Unvaried Email address");
+            return false;
         }
         else {
 
@@ -68,23 +92,22 @@ public class UserModel {
 
                 if (resultState == 1) {
                     System.out.println("Successful Registration");
-                    return new LoginController(this).login();
+                    return true;
                 } else {
                     System.out.println("Failure Registration");
-                    return null;
+                    return false;
                 }
             }
             catch (SQLIntegrityConstraintViolationException ee){
                 System.out.println("This Username/email are registered");
-                return null;
+                return false;
             }
             catch (SQLException ex) {
                 System.out.println("Please try again or call support");
-                return null;
+                return false;
             }
         }
 
-        return null;
     }
 
     private int getResultState() throws SQLException {

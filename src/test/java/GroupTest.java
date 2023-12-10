8c2572dea513b72a6f038cc58429c3d727e47f97
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-12-10 23:17:06 +0300

    create Group testing

--- src/test/java/GroupTest.java
+++ src/test/java/GroupTest.java
@@ -1,17 +1,31 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class GroupTest {
 
     @Test
     public void createGroup(){
+        try {
+            Connection con = ConnectionManager.getConnection();
+            User user = new User(con,149, "demo");
+            boolean groupResult = user.createGroupTest();
+            assertTrue(groupResult);
+            String getIdQuery = "SELECT LAST_INSERT_ID()";
+            ResultSet getIdResult =
+                    con.prepareStatement(getIdQuery).executeQuery();
 
+            if (getIdResult.next()) {
+                Statement statement = con.createStatement();
+                int deleteResult = statement.executeUpdate("delete from agroups where gName = 'demo'");
+                assertTrue(deleteResult > 0);
+            }
+        } catch (SQLException ignored) {
+        }
     }
 
-    @Test
-    @DisplayName("Creating a duplicate name is allowed")
-    public void createDuplicateGroup(){
-
-    }
 }

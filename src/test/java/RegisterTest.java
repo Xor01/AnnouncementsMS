7c6eac39d7a18cb1045b58c513ab738d9c26cb1d
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-12-10 23:18:06 +0300

    remove registered user after completing test case

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,27 +1,40 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterTest {
     @DisplayName("Registering using correct information")
-    @Test public void registerCorrectInformation() {
+    @Test public void registerCorrectInformation() throws SQLException {
         Register register = new Register();
         UserRecord userRecord = new UserRecord("username123",
-                "String firstName",
-                " String lastName",
-                " String password",
+                "firstName",
+                "lastName",
+                "password",
                 "email@testmail.com");
         boolean registerValue = false;
+        Connection con = null;
+        Statement statement = null;
         try {
             registerValue = register.performRegistration(userRecord);
+
+            if (registerValue){
+                con = ConnectionManager.getConnection();
+                statement = con.createStatement();
+                int deleteResult = statement.executeUpdate("delete from users where username = 'username123'");
+                assertEquals(1, deleteResult);
+            }
         } catch (SQLException e) {
             assertThrows(SQLException.class, () -> register.performRegistration(userRecord));
         }
-        // todo: delete the record added for testing
+        finally {
+            assert con != null;
+            con.close();
+            assert statement != null;
+            statement.close();
+        }
         assertTrue(registerValue);
     }
 
